20c5d18382a9c7710f0d39a261c6ee0bb89f1897
Souradeepta88 <souradeeptapaul88@gmail.com>
2026-05-23 01:40:16 +0530

    fix server

--- server/server.js
+++ server/server.js
@@ -15,25 +15,37 @@ io.on('connection', (socket) => {
 
   socket.on('joinRoom', (roomId) => {
     if (!rooms[roomId]) rooms[roomId] = [];
+
     if (rooms[roomId].length >= 2) {
       socket.emit('roomFull');
       return;
     }
+
     rooms[roomId].push(socket.id);
     socket.join(roomId);
+
     const color = rooms[roomId].length === 1 ? 'white' : 'black';
+    
+    console.log('Assigning color: ' + color + ' to ' + socket.id);
     socket.emit('assignColor', color);
+
     if (rooms[roomId].length === 2) {
+      console.log('Starting game in room: ' + roomId);
       io.to(roomId).emit('startGame');
     }
   });
 
   socket.on('move', (data) => {
-    socket.to(data.roomId).emit('opponentMove', data.move);
+    console.log('Move received: ' + JSON.stringify(data));
+    socket.to(data.roomId).emit('opponentMove', data);
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
+    for (let roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+      if (rooms[roomId].length === 0) delete rooms[roomId];
+    }
   });
 });
 
